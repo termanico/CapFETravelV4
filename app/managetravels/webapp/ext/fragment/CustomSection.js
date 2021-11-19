f060f4e4f80e439a468ab1f7dff62e52ee7dbbb8
termanico <klingbeil.thorsten@t-online.de>
2021-11-19 18:55:00 +0100

    u

--- app/managetravels/webapp/ext/fragment/CustomSection.js
+++ app/managetravels/webapp/ext/fragment/CustomSection.js
@@ -1,7 +1,27 @@
-sap.ui.define([], function() {
-    'use strict';
+sap.ui.define(["sap/ui/model/json/JSONModel"], function (JSONModel) {
+    "use strict";
 
     return {
-        predefinedMethod: function() {}
+        onChartSelectionChanged: function (oEvent) {
+            if (oEvent.getParameter("selected")) {
+                // 
+                this.editFlow
+                    .getView()
+                    .setModel(
+                        new JSONModel(oEvent.getParameter("data")[0].data), "popup");
+
+
+                // get Popover from xml fragment dependents
+                this._oPopover = oEvent.getSource().getParent().getDependents()[0];
+                if (this._oPopover) {
+                    // open popover at selected chart segment
+                    this._oPopover.openBy(
+                        oEvent.getParameter("data")[0].target
+                    );
+                }
+            }
+        },
     };
 });
+
+
